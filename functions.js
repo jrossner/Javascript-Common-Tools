fbb3627ca2be926c302db073cc99911d3ab9fc53
Jacob Rossner <66050953+jrossner@users.noreply.github.com>
2022-02-03 11:56:12 -0500

    Update functions.js
    added arrayTotal() function

--- functions.js
+++ functions.js
@@ -1,3 +1,4 @@
+// get root function
 function rooter(value,root = 2) {
   ans = Math.pow(value,1/root)
   return ans
@@ -16,3 +17,8 @@ module.exports = { rooter }
 
 // to import function from another file:
 const {rooter} = require("./function")
+
+// get sum of all elements in a 1D array
+function arrayTotal(array) {
+  return array.reduce(function(a,b) { return a+b }, 0)
+}
