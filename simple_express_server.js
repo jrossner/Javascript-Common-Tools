5eac417b7a112bd08d1eb7c8de03e15c9247f6b6
Jacob Rossner <66050953+jrossner@users.noreply.github.com>
2022-02-16 09:44:50 -0500

    Create simple_express_server.js
    simple server example that takes request query arguments and can display static files

---
+++ simple_express_server.js
@@ -0,0 +1,28 @@
+const ex = require('express')
+const app = ex()
+const port = 8080
+
+app.get('/test', (req ,res) => {
+  res.send("Hit endpoint")
+})
+
+app.get('/add', (req, res) => {
+  a = Number(req.query.a)
+  b = Number(req.query.b)
+  res.send(`The total of ${a} + ${b} is ${a+b}`)
+})
+
+app.use(express.static('demo_visuals'))
+
+app.listen(port, () => {
+  console.log(`Simple App listening on port: ${port}`)
+})
+
+/* example queries:
+
+  localhost:8080/test
+  
+  localhost:8080/add?a=1&b=2
+  
+  localhost:8080/simple.html
+  */
